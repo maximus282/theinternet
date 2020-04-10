e51fc28ecf7fa3e7d73e2cdd962ab6d6ed70a9fc
MakS <max282@mmm.com>
2020-04-10 10:27:40 +0200

    added mac driver path

--- theinternet/config/config.py
+++ theinternet/config/config.py
@@ -5,6 +5,8 @@ if os_name == "Windows":
     chromedriver_path="../driver/windows/chromedriver"
 if os_name == "Linux":
     chromedriver_path = "../driver/linux/chromedriver"
+if os_name == "Darwin":
+    chromedriver_path = "../driver/mac/chromedriver"
 
 base_url="https://the-internet.herokuapp.com/"
 resource_url={
